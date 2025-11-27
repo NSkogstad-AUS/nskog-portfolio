93db450db901f5285edd520f5447f75a69c07bdd
NSkogstad-AUS <nskogstad@student.unimelb.edu.au>
2025-11-27 18:16:31 +1100

    docs: Added explanatory comments to each

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+// This file is used for page-level bootstrap imports/initalises components to assemble the page
 import {renderNavbar} from "./components/navbar.js";
 
 renderNavbar({target: document.querySelector("header"), configUrl: "./data/navigation.json"});
--- public/components/navbar.js
+++ public/components/navbar.js
@@ -1,3 +1,7 @@
+// This is the behavior for the nav, Fetching navigation data, wiring events
+// Also renders.attaches the navbar.html fragment
+// Think of the dynamic states, clicks and toggles (this is .js whereas HTML is the structure)
+
 <div>
     <script src="components/navbar.js" type="module" defer></script>
 </div>
