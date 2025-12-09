6d3c53ca11b553151b4a3387719aecf888c3cf2f
NSkogstad-AUS <nskogstad@student.unimelb.edu.au>
2025-12-09 14:20:03 +1100

    refactor: removing github pages support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")?.[1] ?? "";
-const basePath = isGithubActions && repoName ? `/${repoName}` : "";
-
 const nextConfig = {
   experimental: {
     viewTransition: true,
@@ -10,12 +6,6 @@ const nextConfig = {
   turbopack: {
     root: __dirname,
   },
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath,
-  assetPrefix: basePath || undefined,
 };
 
 module.exports = nextConfig;
