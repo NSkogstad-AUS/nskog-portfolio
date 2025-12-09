639aa760e58c88631a4d3225025186244c5dbda8
NSkogstad-AUS <nskogstad@student.unimelb.edu.au>
2025-12-09 12:35:40 +1100

    feat: finalised the about page content

--- src/app/pages/About/page.tsx
+++ src/app/pages/About/page.tsx
@@ -23,18 +23,44 @@ export default function AboutPage() {
               based in - you guessed it - Melbourne, Australia.
               <br/>
               <br/>
-              Some of my most notable projects are involved in 
+              Some{" "}<a className="card__link" href="projects">notable projects</a>{" "}
+              of mine are involved with the University of Melbourne, like a gui made for the{" "}
+              <a className="card__link" href="https://www.facebook.com/unimelbrover/" target="_blank" rel="noopener noreferrer">Unimelb Rover Team</a> to facilitate rover to team communication,
+              Trained a small CNN to determine fill-level estimation for the Humanoid team in the{" "}
+              <a className="card__link" href="https://www.melbournespace.com.au/" target="_blank" rel="noopener noreferrer">Melbourne Space Program</a>.
+              <br/>
+              <br/>
+              Beyond that, I love home labbing, and have been pursuing designing my own network architecture. Also I love playing around with my Raspberry Pi and Arduino,
+              as well as reading books, watching films, and writing!
+
               <br/>
               <br/>
               Outside of software, I enjoy playing badminton, bouldering, organising/participating in university club activities
-              ranging from computer-science to taekwondo related clubs, as well as spending time with my dog Buddy.
+              ranging from computer-science to taekwondo, as well as spending time with my dog <a className="about--buddy" href="#buddy-info">Buddy</a>.
               I also have a passion for exploring the world in general, stemming from my Norwegian roots! Feel free to contact me if you'd like to connect.
             </p>
+
+            <div className="about--links">
+              <a className="card2__item" href="mailto:nicolai@skogstad.com">
+                <i className="bi bi-envelope" aria-hidden="true" />
+                <span>nicolai@skogstad.com</span>
+              </a>
+              <i className="bi bi-star-fill"/>
+              <a className="card2__item" href="https://www.linkedin.com/in/nicolai-skogstad-8333a221b/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-linkedin" aria-hidden="true" />
+                <span>LinkedIn</span>
+              </a>
+              <i className="bi bi-star-fill"/>
+              <a className="card2__item" href="https://github.com/NSkogstad-AUS" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github" aria-hidden="true"/>
+                <span>GitHub</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="dog-info">
+      <div className="dog-info" id="buddy-info">
         <div className="dog-images">
           <div className="dog-photo">
             <Image
