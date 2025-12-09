0ea52caf1bdd73c6781435c8646b8b7e7b3d8bd2
NSkogstad-AUS <nskogstad@student.unimelb.edu.au>
2025-12-09 14:16:16 +1100

    feat: added github pages support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")?.[1] ?? "";
+const basePath = isGithubActions && repoName ? `/${repoName}` : "";
+
 const nextConfig = {
   experimental: {
     viewTransition: true,
@@ -6,6 +10,12 @@ const nextConfig = {
   turbopack: {
     root: __dirname,
   },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath,
+  assetPrefix: basePath || undefined,
 };
 
 module.exports = nextConfig;
